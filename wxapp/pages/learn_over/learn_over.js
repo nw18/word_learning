9df0e129440fc1a3b49c5b70d2401c75d73b8a9c
HanFengqi <hanfengqi@yqj.cn>
2017-07-06 11:55:35 +0800

    dev：增加单词完成页面

--- wxapp/pages/learn_over/learn_over.js
+++ wxapp/pages/learn_over/learn_over.js
@@ -3,23 +3,32 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {}
+ 
   },
   //事件处理函数
-  bindViewTap: function() {
+  listenAgain: function () {
     wx.navigateTo({
-      url: '../learn_over/learn_over'
+      url: '../logs/logs'
+    })
+  },
+  testAgain: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  practise: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
     })
   },
   onLoad: function () {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
