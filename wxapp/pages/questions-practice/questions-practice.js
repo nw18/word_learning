68f666701b9bda9e04bea154451420d53685a1ac
yangye <931077397@qq.com>
2017-07-11 15:15:19 +0800

    dev:真题练习页的逻辑

--- wxapp/pages/questions-practice/questions-practice.js
+++ wxapp/pages/questions-practice/questions-practice.js
@@ -4,22 +4,19 @@
 var app = getApp()
 Page({
   data: {
+
+
+    quesInfoList:[],
+    index: 0,
+    quesInfo:{},
+
     progressItem:{
       progressNum: 0,
-      progressAll: 20,
+      progressAll: 0,
       progressPercent: 0,
     },
-    answer:"B",
-    answerArray:["A","B","C","D"],
-    index:0,
-    // trueColor:blur,
-    // flaseColor:red,
 
-    content:"could you go shopping wigo shopping with me tomorrgo shopping with me tomorrgo shopping with me tomorrgo shopping with me tomorrgo shopping with me tomorrgo shopping with me tomorrgo shopping with me tomorrth me tomorrow？\n______ my father and i will go wuhan tomorrow",
-    motto: 'Hello World',
-
-    movies: [],
-    resqMD: {}
+    btnClass: "sel-btn-true",
 
   },
 
@@ -91,39 +88,66 @@ Page({
     //   url: '../logs/logs'
     // })
     console.log(e);
+    var that = this
+    if (e.target.id == this.data.quesInfo.AnswerList[0]){
+      console.log("正确");
+      for (var i in this.data.quesInfo.OptionList) {
+        var opt = this.data.quesInfo.OptionList[i];
+        if (e.target.id == opt.KeyName){
 
-    if (e.target.id == this.data.answer){
-      var that = this
-      var perNum = that.data.progressItem.progressNum;
-      perNum++
-      if (perNum > that.data.progressItem.progressAll){
-        perNum = 0;
+          this.data.quesInfo.OptionList[i].state = 1;
+          that.setData({
+            quesInfo: this.data.quesInfo,
+          })
+        }
       }
-      console.log(perNum);
-      that.data.progressItem.progressNum = perNum;
-      that.data.progressItem.progressPercent = perNum / that.data.progressItem.progressAll * 100;
-      that.setData({
-        progressItem: that.data.progressItem,
-      })
+      
+    }else{
+      console.log("错误");
+      for (var i in this.data.quesInfo.OptionList) {
+        var opt = this.data.quesInfo.OptionList[i];
+        if (e.target.id == opt.KeyName) {
+
+          this.data.quesInfo.OptionList[i].state = 2;
+          that.setData({
+            quesInfo: this.data.quesInfo,
+          })
+        }
+      }
     }
 
+  },
+  //填空题点击了查看答案
+  bindTianBtnTap:function(){
+
+    console.log(this.data.quesInfo.AnswerContent);
   },
 
   //点击了下一题
   bindNextBtnTap: function () {
-    // wx.navigateTo({
-    //   url: '../logs/logs'
-    // })
-    var that = this
-    // that.setData({
-    //   progressNum: that.data.progressNum + 1,
-    //   progressPercent: (that.data.progressNum + 1) / that.data.progressAll * 100,
-    // })
 
+    var that = this
+
+    if (that.data.index+1 == that.data.quesInfoList.length){
+      wx.navigateTo({
+        url: '../learn-over/learn-over'
+      })
+      return;
+    }
+    that.data.index++;
     that.setData({
-      answer: that.data.answerArray[that.data.index],
-      // index: (that.data.index + 1) % 4,
-      index: formatNumber(that.data.index),
+      quesInfo: that.data.quesInfoList[that.data.index],
+    })
+
+    var perNum = that.data.progressItem.progressNum;
+    perNum++
+    if (perNum > that.data.progressItem.progressAll) {
+      perNum = 0;
+    }
+    that.data.progressItem.progressNum = perNum;
+    that.data.progressItem.progressPercent = perNum / that.data.progressItem.progressAll * 100;
+    that.setData({
+      progressItem: that.data.progressItem,
     })
   },
   
@@ -132,19 +156,27 @@ Page({
     wx.showLoading({
       title: '加载中',
     })
-    console.log("qqqqqqqqqq"),
       wx.request({
       url: 'https://openapi.yqj.cn/MockAPI/WordLearning/GetQuesList',
         header: {
           'Content-Type': 'application/json'
         },
         success: function (res) {
-          console.log("11111111111111 {{res}}");
            wx.hideLoading({});
-           that.setData({resqMD:res.data})
+           var list = res.data.Data;
+           that.setData({ quesInfoList: list,
+                         quesInfo: list[that.data.index],
+           })
+           var perNum = that.data.index+1;
+           that.data.progressItem.progressNum = perNum;
+           that.data.progressItem.progressAll = list.length;
+           that.data.progressItem.progressPercent = perNum / list.length * 100;
+           that.setData({
+             progressItem: that.data.progressItem,
+           })
+           
         },
         fail: function (res) {
-           console.log("2222222222222"),
             wx.hideLoading({})
         },
 
