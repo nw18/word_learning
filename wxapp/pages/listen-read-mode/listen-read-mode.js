b96146e4f28633743327f32bf131ab5d6a60a995
yangye <931077397@qq.com>
2017-07-14 17:36:44 +0800

    dev:单词练习的UI

--- wxapp/pages/listen-read-mode/listen-read-mode.js
+++ wxapp/pages/listen-read-mode/listen-read-mode.js
@@ -14,7 +14,12 @@ Page({
         progressAll: 0,
         progressPercent: 0,
       },
-
+      cixing: ["adj.圆形的", "n.通知","vt.触摸"],
+      ExtraList:{
+        liju: "Place \nyour hands on your shoulders and move your elbows up, back, and down, in a circular motion\n手置于肩上，肘部向上、向后、向下做圆圈运动。\nBoth sides of the river can be explored on this circular walk\n沿着这条环行线路走一圈，河的两边都可以看到。",
+        shipin: "http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400"
+      },
+      
       src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
 
       picture: 'http://img.zcool.cn/job/groups/b445558d077800000141f02f67a5.jpg',
@@ -41,64 +46,58 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    var that = this;
-    var preTmie = 5;
-    var perNum = that.data.progressItem.progressNum;
-    var rightInterval = setInterval(function () {
+//     var that = this;
+//     var preTmie = 5;
+//     var perNum = that.data.progressItem.progressNum;
+//     var rightInterval = setInterval(function () {
     
-      console.log("这是一个五秒的定时器");
-// 每5秒切换一次单词
-      preTmie--;
-      if(preTmie==-1){
-        perNum++;
-        preTmie=5
-        that.data.index++;
-        // 换单词
-        that.setData({
-          wordInfo: that.data.wordInfoList[that.data.index],
-        });
-        console.log(perNum);
-        that.data.progressItem.progressNum = perNum;
-        that.data.progressItem.progressPercent = perNum / that.data.progressItem.progressAll * 100;
-        that.setData({
-          progressItem: that.data.progressItem,
+//       console.log("这是一个五秒的定时器");
+// // 每5秒切换一次单词
+//       preTmie--;
+//       if(preTmie==-1){
+//         perNum++;
+//         preTmie=5
+//         that.data.index++;
+//         // 换单词
+//         that.setData({
+//           wordInfo: that.data.wordInfoList[that.data.index],
+//         });
+//         console.log(perNum);
+//         that.data.progressItem.progressNum = perNum;
+//         that.data.progressItem.progressPercent = perNum / that.data.progressItem.progressAll * 100;
+//         that.setData({
+//           progressItem: that.data.progressItem,
          
-        })
-      }
+//         })
+//       }
      
-      if (perNum >= that.data.progressItem.progressAll) {
-        clearInterval(rightInterval)
-        wx.redirectTo({
-          url: '../learn-over/learn-over'     
-        })
-        return;
-      }
-      // 更新时间
-    that.setData({
-      time:preTmie
-    })
-   
-
-     
-
-
-
-    }, 1000);
-    that.rightInterval = rightInterval;
+//       if (perNum >= that.data.progressItem.progressAll) {
+//         clearInterval(rightInterval)
+//         wx.redirectTo({
+//           url: '../learn-over/learn-over'     
+//         })
+//         return;
+//       }
+//       // 更新时间
+//     that.setData({
+//       time:preTmie
+//     })
+//     }, 1000);
+//     that.rightInterval = rightInterval;
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+    clearInterval(this.rightInterval);
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-    WxNotificationCenter.removeNotification("testTabNotificationName", this)
+    clearInterval(this.rightInterval);
   },
 
   /**
