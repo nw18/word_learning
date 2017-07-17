d089446dc6b4b760a28417229b66b8d7df62bf00
yangye <931077397@qq.com>
2017-07-17 11:58:09 +0800

    dev:一些图片的更换

--- wxapp/pages/self-evaluation/self-evaluation.js
+++ wxapp/pages/self-evaluation/self-evaluation.js
@@ -20,7 +20,7 @@ Page({
 
     src: 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E06DCBDC9AB7C49FD713D632D313AC4858BACB8DDD29067D3C601481D36E62053BF8DFEAF74C0A5CCFADD6471160CAF3E6A&fromtag=46',
 
-    picture: '../../img/laba.gif',
+    picture: '../../img/voice-big.gif',
     userInfo: {}
   },
 
@@ -43,13 +43,7 @@ Page({
       
     })
   },
-  
-  funended: function () {
-    that.setData({
-      picture: '../../img/laba.png',
-    })
-    console.log("audio end");
-  },
+
   testNotificationFn:function (e){
 
     console.log("接受通知");
@@ -70,7 +64,7 @@ Page({
 
     wx.onBackgroundAudioStop(function () {
       that.setData({
-        picture: '../../img/laba.png',
+        picture: '../../img/voice-big.png',
       });
       console.log('onBackgroundAudioStop')
     })
