a89df26d5352287e31cde6f7de619d6f40bbc006
newind <xf@yqj.cn>
2017-07-21 09:33:23 +0800

    fix listen-read layouot;change learn over all to redirect.

--- wxapp/pages/learn-over/learn-over.js
+++ wxapp/pages/learn-over/learn-over.js
@@ -11,28 +11,27 @@ Page({
   //事件处理函数
   listenAgain: function () {
     var that = this;
-    wx.navigateTo({
-      url: '../listen-read-mode/listen-read-mode?lid=' + that.data.lid + '&mode=' + that.data.mode + '&query=' + that.data.query + '&index=' + that.data.index,
+    wx.redirectTo({
+      url: '../listen-read-mode/listen-read-mode?lid=' + that.data.lid + '&mode=' + that.data.mode + '&query=' + that.data.query + '&index=0'// + that.data.index,
     })
   },
   testAgain: function () {
     var that = this;
     wx.redirectTo({
-      url: '../self-evaluation/self-evaluation?lid=' + that.data.lid + '&mode=' + that.data.mode + '&query=' + that.data.query + '&index=' + that.data.index,
+      url: '../self-evaluation/self-evaluation?lid=' + that.data.lid + '&mode=' + that.data.mode + '&query=' + that.data.query + '&index=0'// + that.data.index,
     })
   },
   practise: function () {
     var that = this;
     wx.redirectTo({
-      url: '../questions-practice/questions-practice?lid=' + that.data.lid + '&mode=' + that.data.mode + '&query=' + that.data.query + '&index=' + that.data.index,
+      url: '../questions-practice/questions-practice?lid=' + that.data.lid + '&mode=' + that.data.mode + '&query=' + that.data.query + '&index=0'// + that.data.index,
     })
   },
   onLoad: function (e) {
     console.log(e.lid);
     this.setData({
       lid: e.lid,
-      // mode: e.mode,
-      mode: 2,
+      mode: e.mode,
       query: e.query,
       index: e.index,
     })
